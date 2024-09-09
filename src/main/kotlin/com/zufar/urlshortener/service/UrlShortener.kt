2693b25930ea3eb087a2582b4e49c26c7d66abed
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-09-09 23:24:53 +0100

    app.base-url was added as env variable

--- src/main/kotlin/com/zufar/urlshortener/service/UrlShortener.kt
+++ src/main/kotlin/com/zufar/urlshortener/service/UrlShortener.kt
@@ -4,6 +4,7 @@ import com.zufar.urlshortener.common.entity.UrlMapping
 import com.zufar.urlshortener.repository.UrlRepository
 import lombok.extern.slf4j.Slf4j
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
@@ -14,7 +15,9 @@ class UrlShortener(
     private val urlValidator: UrlValidator
 ) {
     private val log = LoggerFactory.getLogger(UrlShortener::class.java)
-    private val baseUrl = "http://localhost:8080/"
+
+    @Value("\${app.base-url}")
+    private lateinit var baseUrl: String
 
     fun shortenUrl(originalUrl: String): Mono<String> {
         log.info("Received request to shorten URL: {}", originalUrl)
