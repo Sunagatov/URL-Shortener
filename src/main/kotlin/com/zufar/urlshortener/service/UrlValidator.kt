82a5c3a191651fa8974eed29311d1c58bf75c50f
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-09-29 20:32:37 +0100

    updated validations for original url

--- src/main/kotlin/com/zufar/urlshortener/service/UrlValidator.kt
+++ src/main/kotlin/com/zufar/urlshortener/service/UrlValidator.kt
@@ -8,28 +8,30 @@ private const val MAX_ALLOWED_URL_LENGTH = 2048
 
 @Service
 class UrlValidator {
-
     private val allowedProtocols = setOf("http", "https")
     private val loopbackAddresses = setOf("localhost", "127.0.0.1", "::1", "short-link.zufargroup.com")
-    private val validator = org.apache.commons.validator.routines.UrlValidator()
+    private val validator = org.apache.commons.validator.routines.UrlValidator(allowedProtocols.toTypedArray())
 
     fun validateUrl(url: String) {
         require(url.isNotBlank()) { "URL must not be empty or blank." }
         require(!url.contains(" ")) { "URL must not contain spaces." }
         require(url.length <= MAX_ALLOWED_URL_LENGTH) { "URL exceeds the maximum allowed length of $MAX_ALLOWED_URL_LENGTH characters." }
+        require(hasValidProtocol(url)) { "URL must have a proper scheme (http or https)." }
         require(validator.isValid(url)) { "URL is not valid. Please ensure it has the correct format and syntax." }
-        require(isValidProtocol(url)) { "URL must have a proper scheme (e.g., http or https)." }
         require(isValidHost(url)) { "URL must contain a valid host. Loopback addresses (localhost, 127.0.0.1, ::1, short-link.zufargroup.com) are not allowed." }
     }
 
-    private fun isValidProtocol(url: String): Boolean {
-        val parsedUrl: URL = URI(url).toURL()
-        return parsedUrl.protocol in allowedProtocols
+    private fun hasValidProtocol(url: String): Boolean {
+        return allowedProtocols.any { url.startsWith("$it://") }
     }
 
     private fun isValidHost(url: String): Boolean {
-        val parsedUrl: URL = URI(url).toURL()
-        val host = parsedUrl.host
-        return !host.isNullOrBlank() && host !in loopbackAddresses
+        return try {
+            val parsedUrl: URL = URI(url).toURL()
+            val host = parsedUrl.host
+            !host.isNullOrBlank() && host !in loopbackAddresses
+        } catch (e: Exception) {
+            false
+        }
     }
 }
