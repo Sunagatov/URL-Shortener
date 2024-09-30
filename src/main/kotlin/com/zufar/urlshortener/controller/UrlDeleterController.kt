8a76b9b904879ce8fe53c6f3895da5b7e7118ab2
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-09-30 01:27:28 +0100

    DELETE HTTP REST API was added for url mapping

---
+++ src/main/kotlin/com/zufar/urlshortener/controller/UrlDeleterController.kt
@@ -0,0 +1,60 @@
+package com.zufar.urlshortener.controller
+
+import com.zufar.urlshortener.common.dto.ErrorResponse
+import com.zufar.urlshortener.service.UrlDeleter
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class UrlDeleterController(
+    private val urlDeleter: UrlDeleter
+) {
+    private val log = LoggerFactory.getLogger(UrlDeleterController::class.java)
+
+    @Operation(
+        summary = "Delete a shortened URL",
+        description = "Deletes a URL mapping by its unique hash.",
+        tags = ["URL Management"]
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "204",
+                description = "Successfully deleted the URL mapping."
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "URL mapping not found.",
+                content = [Content(
+                    schema = Schema(implementation = ErrorResponse::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "500",
+                description = "Internal server error",
+                content = [Content(
+                    schema = Schema(implementation = ErrorResponse::class)
+                )]
+            )
+        ]
+    )
+    @DeleteMapping("/url/{urlHash}")
+    fun deleteUrlMapping(
+        @PathVariable urlHash: String
+    ): ResponseEntity<Any> {
+        log.info("Received request to delete URL mapping for urlHash='{}'", urlHash)
+        urlDeleter.deleteUrl(urlHash)
+        log.info("Successfully deleted URL mapping for urlHash='{}'", urlHash)
+        return ResponseEntity.noContent().build()
+    }
+}
---
+++ src/main/kotlin/com/zufar/urlshortener/service/UrlDeleter.kt
@@ -0,0 +1,24 @@
+package com.zufar.urlshortener.service
+
+import com.zufar.urlshortener.repository.UrlRepository
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class UrlDeleter(
+    private val urlRepository: UrlRepository
+) {
+    private val log = LoggerFactory.getLogger(UrlDeleter::class.java)
+
+    fun deleteUrl(urlHash: String) {
+        log.info("Attempting to delete URL mapping for urlHash='{}'", urlHash)
+        if (urlRepository.existsById(urlHash)) {
+            log.info("Found URL mapping for urlHash='{}'. Deleting...", urlHash)
+            urlRepository.deleteById(urlHash)
+            log.info("Successfully deleted URL mapping for urlHash='{}'", urlHash)
+        } else {
+            log.warn("No URL mapping found for urlHash='{}'. Deletion failed.", urlHash)
+            throw IllegalArgumentException("No URL mapping found for urlHash='$urlHash'. Deletion failed.")
+        }
+    }
+}
