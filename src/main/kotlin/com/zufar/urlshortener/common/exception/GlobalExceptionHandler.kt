4f9a969c5dee3a8fdb2ec5df24112a40ae6a3ab9
zufar-sunagatov <zufar.sunagatov@gmail.com>
2024-11-06 13:54:26 +0000

    Fixed the issue: 500 Response HTTP Code when trying to create an account with an existing email

--- src/main/kotlin/com/zufar/urlshortener/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/zufar/urlshortener/common/exception/GlobalExceptionHandler.kt
@@ -1,11 +1,15 @@
 package com.zufar.urlshortener.common.exception
 
+import com.zufar.urlshortener.auth.exception.EmailAlreadyExistsException
+import com.zufar.urlshortener.auth.exception.InvalidTokenException
 import com.zufar.urlshortener.shorten.exception.UrlNotFoundException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.naming.AuthenticationException
 
 private const val LOG_ERROR_MESSAGE = "An unexpected error occurred"
 
@@ -41,5 +45,30 @@ class GlobalExceptionHandler {
         log.error(LOG_ERROR_MESSAGE, ex)
         val errorResponse = ErrorResponse(errorMessage = ex.message ?: "URL not found")
         return ResponseEntity(errorResponse, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(InvalidTokenException::class)
+    fun handleInvalidTokenException(ex: InvalidTokenException): ResponseEntity<ErrorResponse> {
+        log.error("Invalid token: ${ex.message}", ex)
+        val errorResponse = ErrorResponse(errorMessage = ex.message ?: "Invalid token")
+        return ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(org.springframework.security.authentication.BadCredentialsException::class)
+    fun handleBadCredentialsException(ex: BadCredentialsException): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(errorMessage = "Invalid email or password")
+        return ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(AuthenticationException::class)
+    fun handleAuthenticationException(ex: AuthenticationException): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(errorMessage = "Authentication failed")
+        return ResponseEntity(errorResponse, HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException::class)
+    fun handleEmailAlreadyExistsException(ex: EmailAlreadyExistsException): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(errorMessage = ex.message ?: "Email already in use")
+        return ResponseEntity(errorResponse, HttpStatus.CONFLICT)
     }
 }
