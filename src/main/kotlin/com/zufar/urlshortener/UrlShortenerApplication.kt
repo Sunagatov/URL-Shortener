aeaeba675dae65a01b2365b21316fe66b7ba67c4
Reyzis2021 <reyzarium@gmail.com>
2024-11-24 14:26:59 +0400

    SHORTY-63 Fix deletion url by expiration time Job

--- src/main/kotlin/com/zufar/urlshortener/UrlShortenerApplication.kt
+++ src/main/kotlin/com/zufar/urlshortener/UrlShortenerApplication.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @OpenAPIDefinition(
     info = Info(
@@ -13,6 +14,7 @@ import org.springframework.boot.runApplication
     )
 )
 @SpringBootApplication
+@EnableScheduling
 class UrlShortenerApplication
 
 fun main(args: Array<String>) {
