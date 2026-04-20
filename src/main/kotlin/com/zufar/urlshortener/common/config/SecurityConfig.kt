2b4e4bf324ee89273dfa82d4155236f33df0afb0
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-04-20 02:44:13 +0100

    update

--- src/main/kotlin/com/zufar/urlshortener/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/zufar/urlshortener/common/config/SecurityConfig.kt
@@ -5,54 +5,59 @@ import com.zufar.urlshortener.auth.service.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableMethodSecurity
 class SecurityConfig(
     private val customUserDetailsService: CustomUserDetailsService,
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     @Bean
     fun authenticationProvider(): DaoAuthenticationProvider {
-        val authProvider = DaoAuthenticationProvider()
-        authProvider.setUserDetailsService(customUserDetailsService)
-        authProvider.setPasswordEncoder(passwordEncoder())
+        val authProvider = DaoAuthenticationProvider(customUserDetailsService)
+        authProvider.setPasswordEncoder(passwordEncoder)
         return authProvider
     }
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
-    @Throws(Exception::class)
     fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
         return authenticationConfiguration.authenticationManager
     }
 
     @Bean
-    @Throws(Exception::class)
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { csrf -> csrf.disable() }
+        http
+            .csrf { it.disable() }
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers(HttpMethod.POST, "/api/v1/urls").permitAll()
-                    .requestMatchers("/api/v1/auth/**", "/v1/auth/**", "/url/**", "/api/v1/swagger-ui/**", "/api/v1/api-docs/**", "/api/v1/health").permitAll()
+                    .requestMatchers(
+                        "/api/v1/health",
+                        "/api/v1/auth/**",
+                        "/v1/auth/**",
+                        "/url/**",
+                        "/api/v1/swagger-ui/**",
+                        "/api/v1/api-docs/**"
+                    ).permitAll()
                     .anyRequest().authenticated()
             }
             .authenticationProvider(authenticationProvider())
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+
         return http.build()
     }
 }
