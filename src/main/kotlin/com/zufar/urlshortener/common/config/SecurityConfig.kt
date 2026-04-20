6f35bcc13d689132b3d52c333e0fdb772fa593c2
zufar-sunagatov <zufar.sunagatov@gmail.com>
2026-04-20 02:41:55 +0100

    update

--- src/main/kotlin/com/zufar/urlshortener/common/config/SecurityConfig.kt
+++ src/main/kotlin/com/zufar/urlshortener/common/config/SecurityConfig.kt
@@ -48,7 +48,7 @@ class SecurityConfig(
             .authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers(HttpMethod.POST, "/api/v1/urls").permitAll()
-                    .requestMatchers("/api/v1/auth/**", "/v1/auth/**", "/url/**", "/api/v1/swagger-ui/**", "/api/v1/api-docs/**").permitAll()
+                    .requestMatchers("/api/v1/auth/**", "/v1/auth/**", "/url/**", "/api/v1/swagger-ui/**", "/api/v1/api-docs/**", "/api/v1/health").permitAll()
                     .anyRequest().authenticated()
             }
             .authenticationProvider(authenticationProvider())
